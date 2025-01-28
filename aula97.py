0bd696d9f1e99672432683bbeff5669a87ed2e97
Bruno Batista <brunodaflon1@gmail.com>
2025-01-28 19:27:50 -0300

    Add files via upload

---
+++ aula97.py
@@ -0,0 +1,16 @@
+# Entendendo os seus próprios módulos Python
+# O primeiro módulo executado chama-se __main__
+# Você pode importar outro módulo inteiro ou parde do módulo
+# O python conhece a pasta onde o __main__ está e as pastas
+# abaixo dele
+# Ele não reconhece pastar e módulos acima do __main__ por 
+# padrão
+# O python conhece todos os módulos e pacotes presentes
+
+import aula97_m
+from aula97_m import soma, variavel_modulo
+# print('Este módulo se chama', __name__)
+print(aula97_m.variavel_modulo)
+print(variavel_modulo)
+print(soma(2,3))
+print(aula97_m.soma(2,3))
